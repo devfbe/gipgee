aa2231566df4c18ae6902705ea2cb140e87bf18a
Felix Becker <git@felixbecker.name>
2022-07-15 01:54:20 +0200

    Fix wrong exec command location
    fix wrong location of the ExecCmd (was added to selfrelease cli instead
    of the root location of the cli).

--- main.go
+++ main.go
@@ -13,10 +13,21 @@ var cli struct {
 	SelfRelease selfrelease.SelfReleaseCmd `cmd:""`
 	UpdateCheck updatecheck.UpdateCheckCmd `cmd:""`
 	ImageBuild  imagebuild.ImageBuildCmd   `cmd:""`
+	Exec        ExecCmd                    `cmd:""`
 }
 
 func main() {
 	ctx := kong.Parse(&cli)
 	err := ctx.Run()
 	ctx.FatalIfErrorf(err)
+}
+
+// Below are the exec commands. The gipgee uses this commands to execute commands like the
+// update check command, defined in the image config / defaults. This is safer than trying to
+// render commands to the yaml file (you have to handle escaping and quoting for the shell in yaml then)
+// because you can just run a process and pass the args as slice to the command instead of serializing
+// them to one string in the yaml file.
+type ExecCmd struct {
+	UpdateCheck      updatecheck.ExecUpdateCheckCmd     `cmd:""`
+	StagingImageTest imagebuild.ExecStagingImageTestCmd `cmd:""`
 }
--- selfrelease/cli.go
+++ selfrelease/cli.go
@@ -1,10 +1,5 @@
 package selfrelease
 
-import (
-	"github.com/devfbe/gipgee/imagebuild"
-	"github.com/devfbe/gipgee/updatecheck"
-)
-
 type GenerateKanikoDockerAuthCmd struct {
 	Target string `enum:"staging,release" required:""`
 }
@@ -15,19 +10,8 @@ type GeneratePipelineCmd struct {
 type SelfReleaseCmd struct {
 	GenerateKanikoDockerAuth GenerateKanikoDockerAuthCmd `cmd:""`
 	GeneratePipeline         GeneratePipelineCmd         `cmd:""`
-	Exec                     ExecCmd                     `cmd:""`
 }
 
 const (
 	kanikoSecretsFilename = "gipgee-kaniko-auth.json" // #nosec G101
 )
-
-// Below are the exec commands. The gipgee uses this commands to execute commands like the
-// update check command, defined in the image config / defaults. This is safer than trying to
-// render commands to the yaml file (you have to handle escaping and quoting for the shell in yaml then)
-// because you can just run a process and pass the args as slice to the command instead of serializing
-// them to one string in the yaml file.
-type ExecCmd struct {
-	UpdateCheck      updatecheck.ExecUpdateCheckCmd     `cmd:""`
-	StagingImageTest imagebuild.ExecStagingImageTestCmd `cmd:""`
-}

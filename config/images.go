e128bf691d8e1311dece35f6d15a0d26bb1a7d24
Felix Becker <git@felixbecker.name>
2022-08-21 19:54:29 +0200

    Set self release images to afriserver.de registry
    use private container registry for gipgee self release to
    avoid trouble with dockerhub rate limiting.

--- config/images.go
+++ config/images.go
@@ -4,9 +4,9 @@ import (
 	pm "github.com/devfbe/gipgee/pipelinemodel"
 )
 
-var GolangImage = pm.ContainerImageCoordinates{Registry: "docker.io", Repository: "golang", Tag: "1.18.3"}
-var AlpineImage = pm.ContainerImageCoordinates{Registry: "docker.io", Repository: "alpine", Tag: "latest"}
-var LinterImage = pm.ContainerImageCoordinates{Registry: "docker.io", Repository: "golangci/golangci-lint", Tag: "v1.46.2"}
-var SecurityScannerImage = pm.ContainerImageCoordinates{Registry: "docker.io", Repository: "securego/gosec", Tag: "2.12.0"}
-var KanikoImage = pm.ContainerImageCoordinates{Registry: "gcr.io", Repository: "kaniko-project/executor", Tag: "v1.8.1-debug"}
-var SkopeoImage = pm.ContainerImageCoordinates{Registry: "quay.io", Repository: "skopeo/stable", Tag: "v1.8.0"}
+var GolangImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "golang", Tag: "1.18.3"}
+var AlpineImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "alpine", Tag: "latest"}
+var LinterImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "golangci/golangci-lint", Tag: "v1.46.2"}
+var SecurityScannerImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "securego/gosec", Tag: "2.12.0"}
+var KanikoImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "kaniko-project/executor", Tag: "v1.8.1-debug"}
+var SkopeoImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "skopeo/stable", Tag: "v1.8.0"}

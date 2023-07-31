3861eb12912fc3f074c3bb3336ad3d79dfbb7223
Felix Becker <git@felixbecker.name>
2023-08-01 00:52:53 +0200

    Bump Kaniko Version
    bump kaniko to 1.13.0

--- config/images.go
+++ config/images.go
@@ -8,5 +8,5 @@ var GolangImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afri
 var AlpineImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "alpine", Tag: "latest"}
 var LinterImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "golangci/golangci-lint", Tag: "v1.46.2"}
 var SecurityScannerImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "securego/gosec", Tag: "2.12.0"}
-var KanikoImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "kaniko-project/executor", Tag: "v1.8.1-debug"}
+var KanikoImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "kaniko-project/executor", Tag: "v1.13.0-debug"}
 var SkopeoImage = pm.ContainerImageCoordinates{Registry: "containerregistry.afriserver.de:5000", Repository: "skopeo/stable", Tag: "v1.8.0"}
